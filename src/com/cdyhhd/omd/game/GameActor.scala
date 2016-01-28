c27325ac2d4e95f6ce42e89b8d4ca1c61c9e5b23
wangboo <wangboo@>
2016-01-28 20:33:22 +0800

    session

---
+++ src/com/cdyhhd/omd/game/GameActor.scala
@@ -0,0 +1,12 @@
+package com.cdyhhd.omd.game
+
+import akka.actor.ActorLogging
+import akka.actor.Actor
+
+class GameActor extends Actor with ActorLogging {
+  
+  def recieve = {
+    
+  }
+  
+}
--- src/com/cdyhhd/omd/gate/SessionHandler.scala
+++ src/com/cdyhhd/omd/gate/SessionHandler.scala
@@ -5,11 +5,14 @@ import akka.actor.ActorSystem
 import akka.actor.ActorSystem
 import org.apache.mina.core.session.IoSession
 import org.apache.mina.core.session.IdleStatus
+import com.cdyhhd.omd.game.GameActor
+import akka.actor.Props
 
 class SessionHandler(val system: ActorSystem) extends IoHandlerAdapter {
   
   override def sessionCreated(s: IoSession) {
     
+    system.actorOf(Props[GameActor], "game_user")
   }
   
   override def sessionOpened(s: IoSession) {
@@ -31,4 +34,25 @@ class SessionHandler(val system: ActorSystem) extends IoHandlerAdapter {
   override def messageReceived(s: IoSession, msg: Any) {
     
   }
+}
+
+object SessionHandler {
+  
+  val sessions: Map[Int, IoSession] = Map()
+  var id = 0
+  
+  def generateId(s: IoSession) = synchronized {
+    while(true) {
+      id += 1
+      if(!sessions.contains(id)) {
+        sessions += (id -> s)
+      }
+    }
+    
+  }
+  
+  def releaseId = synchronized {
+    
+  }
+  
 }
